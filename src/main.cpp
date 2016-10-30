6c8f90ad97314dfe63414cc27a7af53a54fbceb6
Jamie Hegarty <Jamiehegarty@hotmail.com>
2016-10-30 01:42:06 +0000

    First functioning state for the application, very much work in progress and rough around the edges, function over form at the moment. Updated images and added a font also

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,20 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstring> 
+
 #include <SFML/Graphics.hpp>
 
-#include "config.h"
+/* TODO 
+	- Tidy up functions (maybe seperate out into a header file)
+	- Make input more robust
+	- Stop monsters stacking if they have the same spawn
+	- Determine potential player safespots
+	- Add nicer fields for user input
+	- Add more comments
+*/
+
+
+//#include "config.h"
 using namespace std;
 
 //Should probably go in header
@@ -14,7 +25,11 @@ const sf::Vector2f WINDOW_SIZE(500.0f, 500.0f); //Predetermined window size
 
 sf::Texture backgroundTexture; //Variable to hold background texture
 sf::Sprite backgroundSprite;
+sf::Text waveText;
+sf::Font mainFont;
+
 sf::RenderWindow gameWindow; //The main game window
+vector<sf::Sprite> gameSprites;
 
 sf::Texture tzKihTexture;
 sf::Texture tzKekTexture;
@@ -23,50 +38,43 @@ sf::Texture YtMejKotTexture;
 sf::Texture ketZekTexture;
 sf::Texture tzTokJadTexture;
 
-sf::Sprite tzKihSprite;
-sf::Sprite tzKekSprite;
-sf::Sprite tokXilSprite;
-sf::Sprite YtMejKotSprite;
-sf::Sprite ketZekSprite;
-sf::Sprite tzTokJadSprite;
- 
+
 static bool sfmlSetup(int start, int wave) {
   
     //Load in required resources
-    if (!backgroundTexture.loadFromFile("../images/Fight_Caves_Map.png")) {
+    if (!backgroundTexture.loadFromFile("../../images/Fight_Caves_Map.png")) {
             std::cerr << "Could not load background texture" << std::endl;
             return false;
-    } else if (!tzKihTexture.loadFromFile("../images/Tz-Kih.png")) {
+    } else if (!tzKihTexture.loadFromFile("../../images/Tz-Kih.png")) {
             std::cerr << "Could not load tzKihTexture texture" << std::endl;
             return false;
-    } else if (!tzKekTexture.loadFromFile("../images/Tz-Kek.png")) {
+    } else if (!tzKekTexture.loadFromFile("../../images/Tz-Kek.png")) {
             std::cerr << "Could not load Tz-Kek texture" << std::endl;
             return false;
-    } else if (!tokXilTexture.loadFromFile("../images/Tok-Xil.png")) {
+    } else if (!tokXilTexture.loadFromFile("../../images/Tok-Xil.png")) {
             std::cerr << "Could not load Tok-Xil texture" << std::endl;
             return false;
-    } else if (!YtMejKotTexture.loadFromFile("../images/Yt-MejKot.png")) {
+    } else if (!YtMejKotTexture.loadFromFile("../../images/Yt-MejKot.png")) {
             std::cerr << "Could not load Yt-MejKot texture" << std::endl;
             return false;
-    } else if (!ketZekTexture.loadFromFile("../images/Ket-Zek.png")) {
+    } else if (!ketZekTexture.loadFromFile("../../images/Ket-Zek.png")) {
             std::cerr << "Could not load ketZekTexture texture" << std::endl;
             return false;
-    } else if (!tzTokJadTexture.loadFromFile("../images/TzTok-Jad.png")) {
+    } else if (!tzTokJadTexture.loadFromFile("../../images/TzTok-Jad.png")) {
             std::cerr << "Could not load tzTokJadTexture texture" << std::endl;
             return false;
-    }
-    
-    //Load Monster sprites here too
+    }else if (!mainFont.loadFromFile("../../fonts/Share-TechMono.ttf")) {
+		std::cerr << "Could not load main font" << std::endl;
+		return false;
+	}
     
     backgroundSprite.setTexture(backgroundTexture);
-    tzKihSprite.setTexture(tzKihTexture);
-    tzKekSprite.setTexture(tzKekTexture);
-    tokXilSprite.setTexture(tokXilTexture);
-    YtMejKotSprite.setTexture(YtMejKotTexture);
-    ketZekSprite.setTexture(ketZekTexture);
-    tzTokJadSprite.setTexture(tzTokJadTexture);
-    
-    backgroundSprite.setScale( //Scale the background sprite to fit the current window size
+	waveText.setFont(mainFont);
+	waveText.setCharacterSize(24);
+	waveText.setColor(sf::Color::Cyan);
+	waveText.setPosition(225, 465);
+   
+	backgroundSprite.setScale( //Scale the background sprite to fit the current window size
             WINDOW_SIZE.x / backgroundSprite.getLocalBounds().width,
             WINDOW_SIZE.y / backgroundSprite.getLocalBounds().height);
 
@@ -77,23 +85,73 @@ static bool sfmlSetup(int start, int wave) {
 }
 
 int printspawn (int wave) {
-  if (wave == 63) { printf("JAD "); return 0; }
-  if (wave >= 31) { printf("360 "); return wave-31; }
-  if (wave >= 15) { printf("180 "); return wave-15; }
-  if (wave >= 7) { printf("90 "); return wave-7; }
-  if (wave >= 3) { printf("45 "); return wave-3; }
+  if (wave == 63) { 
+	  printf("JAD ");
+	  sf::Sprite tzTokJadSprite(tzTokJadTexture);
+	  gameSprites.push_back(tzTokJadSprite);
+
+	  return 0; 
+  }
+  if (wave >= 31) { 
+	  printf("360 "); 
+	  sf::Sprite ketZekSprite(ketZekTexture);
+	  gameSprites.push_back(ketZekSprite);
+
+	  return wave-31; 
+  }
+  if (wave >= 15) { 
+	  printf("180 ");
+	  sf::Sprite YtMejKotSprite(YtMejKotTexture);
+	  gameSprites.push_back(YtMejKotSprite);
+
+	  return wave - 15;
+  }
+  if (wave >= 7) {
+	  printf("90 "); 
+	  sf::Sprite tokXilSprite(tokXilTexture);
+	  gameSprites.push_back(tokXilSprite);
+
+	  return wave-7; 
+  }
+  if (wave >= 3) {
+	  printf("45 ");
+	  sf::Sprite tzKekSprite(tzKekTexture);
+	  gameSprites.push_back(tzKekSprite);
+
+	  return wave-3; 
+  }
+
   printf("22 ");
+  sf::Sprite tzKihSprite(tzKihTexture);
+  gameSprites.push_back(tzKihSprite);
+
   return wave-1;
 }
  
 void getspawn (int loc) {
   int spawnarr[15] = {3, 5, 2, 1, 5, 3, 4, 1, 2, 3, 5, 4, 1, 2, 4};
   int sval = spawnarr[loc];
-  if (sval == 1) printf("NW; ");
-  if (sval == 2) printf("C; ");
-  if (sval == 3) printf("SE; ");
-  if (sval == 4) printf("S; ");
-  if (sval == 5) printf("SW; ");
+
+  if (sval == 1) {
+	  gameSprites.back().setPosition(85, 85);
+	  printf("NW; ");
+  }
+  if (sval == 2) {
+	  gameSprites.back().setPosition(220, 210);
+	  printf("C; ");
+  }
+  if (sval == 3) {
+	  gameSprites.back().setPosition(390, 270);
+	  printf("SE; ");
+  }
+  if (sval == 4) {
+	  gameSprites.back().setPosition(260, 350);
+	  printf("S; ");
+  }
+  if (sval == 5) {
+	  gameSprites.back().setPosition(85, 345);
+	  printf("SW; ");
+  }
   return;
 }
  
@@ -138,7 +196,21 @@ int spawnarr2start (int spawnarr[4]) {
 }
 
 void redrawMap(int start, int wave) {
+	gameSprites.clear();
+	std::string curWaveText("Wave:" + std::to_string(wave));
+	waveText.setString(curWaveText);
+
     printwave(start,wave);
+
+	gameWindow.clear(); //Clear and redraw the screen
+	gameWindow.draw(backgroundSprite);
+
+	for (sf::Sprite sprite : gameSprites) {
+		gameWindow.draw(sprite);
+	}
+
+	gameWindow.draw(waveText);
+	gameWindow.display();
 }
 
 int main() {
@@ -169,40 +241,35 @@ int main() {
     printf("\n");
 
     if (sfmlSetup(start, wave)) {
-        while (gameWindow.isOpen()) {
-            sf::Event event;
+		while (gameWindow.isOpen()) {
+			sf::Event event;
             
             
-            while (gameWindow.pollEvent(event)) {
-                    if (event.type == sf::Event::Closed)
-                            gameWindow.close();
+			while (gameWindow.pollEvent(event)) {
+				if (event.type == sf::Event::Closed)
+					gameWindow.close();
                     
-                    if (event.type == sf::Event::KeyReleased)
-                    {
-                        if (event.key.code == sf::Keyboard::Right)
-                        {
-                          if(wave<63){
-                            wave++;
-                            start++;
-                            redrawMap(start, wave);
-                          }
-                        }
-                        if (event.key.code == sf::Keyboard::Left)
-                        {
-                          if(wave>1){
-                            wave--;
-                            start--;
-                            redrawMap(start, wave);
-                          }
-                        }
-                    }
-            }
-
-            gameWindow.clear(); //Clear and redraw the screen
-            gameWindow.draw(backgroundSprite);
-            //gameWindow.draw(text);
-            gameWindow.display();
-        }
+				if (event.type == sf::Event::KeyReleased)
+				{
+					if (event.key.code == sf::Keyboard::Right)
+					{
+						if(wave<63){
+						wave++;
+						start++;
+						redrawMap(start, wave);
+						}
+					}
+					if (event.key.code == sf::Keyboard::Left)
+					{
+						if(wave>1){
+						wave--;
+						start--;
+						redrawMap(start, wave);
+						}
+					}
+				}
+			}
+		}
     }
     
     return 0;
